b0c90403fd1c440407d2fb0a75bd05f19c37cbc8
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-03-30 19:27:32 +0500

    file upload complete

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import useStorage from "../hooks/useStorage";
 
 function ProgressBar({file, setFile}) {
 
     const {url, progress} =useStorage(file);
 
+    useEffect(()=> {
+        if(url){
+            setFile(null);
+        }
+    },[url, setFile])
+
     return (
-        <div className="progress-bar">
-            {progress}
+        <div className="progress-bar" style={{
+            width: progress + "%"
+        }}>
+
         </div>
     );
 }
