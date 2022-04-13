1b0fa3feb3fdacb7422ae4ef7c5f4fe4bccaa550
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-04-14 01:12:39 +0500

    minor UI fixes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function Modal(props) {
     return (
-        <div className={'backdrop'} onClick={() => props.updateImg(null)}>
+        <div className={'backdrop'} onClick={(e) => {
+            if (e.target.classList.contains('backdrop')){
+                props.updateImg(null);
+            }
+        }}>
             <img src={props.url} alt="IMG"/>
         </div>
     );
