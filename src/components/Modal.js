d3d5296034b5539dc010bb20b8cefec8903adbfd
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-04-14 13:13:40 +0500

    added Delete Button

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -11,13 +11,16 @@ function Modal(props) {
                 }
             }}
             initial={{opacity: 0}}
-            animate={{opacity:1}}
+            animate={{opacity: 1}}
         >
             <motion.img
                 src={props.url}
                 alt="IMG"
-            initial={{y:"-100vh"}}
-            animate={{y:"0vh"}}/>
+                initial={{y: "-100vh"}}
+                animate={{y: "0vh"}}/>
+            <div className="center-content">
+                <button className="del">Delete</button>
+            </div>
         </motion.div>
     );
 }
