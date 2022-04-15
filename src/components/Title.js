7c15f642de774db24c47cc4ad4a3e52f8c86f10a
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-04-15 15:12:33 +0500

    minor UI fixes

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>React Gallery</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Your Personal Firebase Gallery.</p>
     </div>
   )
 }
